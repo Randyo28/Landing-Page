9ba95412d7ee6e1e7ab2eb67951709eb501ac421
Randy Ortiz <Randyortiz28@gmail.com>
2020-10-16 14:40:35 -0400

    fixes

--- public/js/main.js
+++ public/js/main.js
@@ -1,76 +1,76 @@
 /*jshint esversion: 6 */ 
-  let tl = gsap.timeline();
+//   let tl = gsap.timeline();
 
-  tl.fromTo('.logo-area',{
-    x: -200,
-    opacity: 0
-  },{
-    x:0,
-    opacity: 1,
-    delay: '.5',
-    duration: 1.3
-  });
+//   tl.fromTo('.logo-area',{
+//     x: -200,
+//     opacity: 0
+//   },{
+//     x:0,
+//     opacity: 1,
+//     delay: '.5',
+//     duration: 1.3
+//   });
 
-  tl.fromTo('ul',{
-    x: 200,
-    opacity: 0
-  },{
-    x:0,
-    opacity: 1,
-    delay: '.5',
-    duration: 1.3
-  },
-  ">-1.8"
-);
+//   tl.fromTo('ul',{
+//     x: 200,
+//     opacity: 0
+//   },{
+//     x:0,
+//     opacity: 1,
+//     delay: '.5',
+//     duration: 1.3
+//   },
+//   ">-1.8"
+// );
 
-tl.fromTo('.stag',{
-  y: 200,
-  opacity: 0,
-},{
-  y:0,
-  opacity: 1,
-  // delay: '.1',
-  // duration: 1.3,
-  stagger: '.5',
-});
+// tl.fromTo('.stag',{
+//   y: 200,
+//   opacity: 0,
+// },{
+//   y:0,
+//   opacity: 1,
+//   // delay: '.1',
+//   // duration: 1.3,
+//   stagger: '.5',
+// });
 
-tl.fromTo('.shape-1',{
-  y:-220
-},{
-  y:-150,
-  yoyo: true,
-  repeat: -1,
-  delay: -1,
-  duration: 1.5,
-  ease: 'sine.inOut'
-},
-'>-1.8'
-);
+// tl.fromTo('.shape-1',{
+//   y:-220
+// },{
+//   y:-150,
+//   yoyo: true,
+//   repeat: -1,
+//   delay: -1,
+//   duration: 1.5,
+//   ease: 'sine.inOut'
+// },
+// '>-1.8'
+// );
 
-tl.fromTo('.shape-2',{
-  y:370
-},{
-  y:300,
-  yoyo: true,
-  repeat: -1,
-  delay: -1,
-  duration: 1.5,
-  ease: 'sine.inOut'
-},
-'>-0.8'
-);
+// tl.fromTo('.shape-2',{
+//   y:370
+// },{
+//   y:300,
+//   yoyo: true,
+//   repeat: -1,
+//   delay: -1,
+//   duration: 1.5,
+//   ease: 'sine.inOut'
+// },
+// '>-0.8'
+// );
 
-tl.fromTo('.shape-3',{
-  // opacity:0,
-  // backgroundcolor: beige,
-  x:0,
-},{
-  x:70,
-  repeat: -1,
-  // opacity:1,
-  duration: 1.5,
-  yoyo:true,
-  ease: 'sine.inOut'
-},
-'>-1.3'
-);
+// tl.fromTo('.shape-3',{
+//   // opacity:0,
+//   // backgroundcolor: beige,
+//   x:0,
+// },{
+//   x:70,
+//   repeat: -1,
+//   // opacity:1,
+//   duration: 1.5,
+//   yoyo:true,
+//   ease: 'sine.inOut'
+// },
+// '>-1.3'
+// );
