10e88c5744dd1818c04f24a4907e5efd776e4476
Randy Ortiz <Randyortiz28@gmail.com>
2020-10-28 13:25:45 -0400

    fixed error in class names and aligned shapes correctly

--- public/js/main.js
+++ public/js/main.js
@@ -157,6 +157,146 @@ const initialPageAnimation = () => {
 };
 
 
+const midScreen = () => {
+  let tl = gsap.timeline();
+
+  tl.fromTo('.logo',{
+    x: -200,
+    opacity: 0
+  },{
+    x:0,
+    opacity: 1,
+    delay: '.5',
+    duration: 1.3
+  });
+
+  tl.fromTo('.menu',{
+    x: 200,
+    opacity: 0
+  },{
+    x:0,
+    opacity: 1,
+    delay: '.5',
+    duration: 1.3
+  },
+  ">-1.8"
+  );
+
+  tl.fromTo('.info-section h1',{
+    x: 0,
+    y:100,
+    opacity: 0,
+  },{
+    x:0,
+    y:0,
+    opacity: 1,
+    duration: 1.3
+  });
+  tl.addLabel("h1Show");
+
+  tl.fromTo('.girl',{
+    height: 0
+  },{
+    height:200,
+    duration: 1.4,
+    ease:'power2.inOut'
+  },
+  ">-.5"
+  );
+
+  tl.fromTo('.marketing',{
+    height: 0
+  },{
+    height:200,
+    duration: 1.4,
+    ease:'power2.inOut'
+  },
+  ">-1"
+  );
+      //Shape1
+  tl.fromTo('.shape1',{
+    x: -200,
+    opacity:0
+  },{
+    x: -100,
+    opacity: 0.3,
+    duration:1.5,
+    yoyo: true,
+    ease:'sine.inOut',
+    repeat: -1,
+  },
+  ">-1"
+  );
+
+      //Shape3
+  tl.fromTo('.shape3',{
+    opacity:0
+  },{
+    opacity:0.3,
+    duration:1,
+    ease:'power2.inOut'
+  },
+  ">-.2"
+  );
+
+  tl.fromTo('.shape3',{
+    y:400,
+    x:-90
+  },{
+    y:350,
+    duration: 1.5,
+    repeat: -1,
+    yoyo: true,
+    ease:'sine.inOut'
+  }
+  );
+
+  //Shape2
+  tl.fromTo('.shape2',{
+  opacity:0
+  },{
+  opacity:0.3,
+  duration:0.5,
+  ease:'power2.inOut'
+  },
+  ">-2"
+  );
+
+  tl.fromTo('.shape2',{
+  x:550,
+  y:-150,
+  },{
+  y:-185,
+  duration: 1.5,
+  repeat: -1,
+  yoyo: true,
+  ease:'sine.inOut'
+  }
+  );
+
+  tl.fromTo('.info-section h4',{
+    x: 50,
+    opacity: 0
+  },{
+    x:0,
+    opacity: 1,
+    duration: 1.3
+  },
+  "h1Show"
+  );
+
+  tl.fromTo('.call-actions',{
+    x: 50,
+    opacity: 0
+  },{
+    x:0,
+    opacity: 1,
+    duration: 1.3
+  },
+  "h1Show+=1"
+  );
+};
+
 const mobile = () => {
   let tl = gsap.timeline();
 
@@ -299,6 +439,8 @@ const mobile = () => {
 
 if (window.screen.width <= 375){
   mobile();
-} else{
+} else if(window.screen.width <= 500){
+  midScreen();
+}else{
   initialPageAnimation();
 }
