154b26394ce4982efc5424afda26dc7ed05f6b87
Randy Ortiz <Randyortiz28@gmail.com>
2020-11-01 20:37:01 -0500

    finished side-nav

--- public/js/main.js
+++ public/js/main.js
@@ -1,21 +1,32 @@
 
       //Nav-bar//
 
-      function navBar() {
-        var x = document.getElementById("navbar");
-        if (x.style.display === "flex") {
-            x.classList.toggle("change");
-            x.style.display = "none";
-    } else {
-            x.style.display = "flex";
-            }
-          }
+
+      function openNav() {
+        document.getElementById("mySidenav").style.width = "250px";
+      }
+      
+      function closeNav() {
+        document.getElementById("mySidenav").style.width = "0";
+      }
+
+
+
+  //     function navBar() {
+  //       var x = document.getElementById("navbar");
+  //       if (x.style.display === "flex") {
+  //           x.classList.toggle("change");
+  //           x.style.display = "none";
+  //   } else {
+  //           x.style.display = "flex";
+  //           }
+  //         }
   
   
-            //Hamburger-transition
-          function myFunction(x) {
-    x.classList.toggle("change");
-  }
+  //           //Hamburger-transition
+  //         function myFunction(x) {
+  //   x.classList.toggle("change");
+  // }
 
 /* Page transitions Animations */
 
@@ -32,11 +43,11 @@ const initialPageAnimation = () => {
     duration: 1.3
   });
 
-  tl.fromTo('.menu',{
-    x:500,
+  tl.fromTo('.ham-container',{
+    x:100,
     opacity: 0
   },{
-    x:400,
+    x:0,
     opacity: 1,
     delay: '.5',
     duration: 1.3
@@ -59,7 +70,7 @@ const initialPageAnimation = () => {
   tl.fromTo('.girl',{
     height: 0
   },{
-    height:600,
+    height:500,
     duration: 1.4,
     ease:'power2.inOut'
   },
@@ -69,7 +80,7 @@ const initialPageAnimation = () => {
   tl.fromTo('.marketing',{
     height: 0
   },{
-    height:600,
+    height:500,
     duration: 1.4,
     ease:'power2.inOut'
   },
@@ -160,7 +171,7 @@ const initialPageAnimation = () => {
 };
 
 
-const midScreen = () => {
+const midScreen500 = () => {
   let tl = gsap.timeline();
 
   tl.fromTo('.logo',{
@@ -244,7 +255,147 @@ const midScreen = () => {
 
   tl.fromTo('.shape3',{
     y:400,
-    x:-90
+    x:-200
+  },{
+    y:350,
+    duration: 1.5,
+    repeat: -1,
+    yoyo: true,
+    ease:'sine.inOut'
+  }
+  );
+
+  //Shape2
+  tl.fromTo('.shape2',{
+  opacity:0
+  },{
+  opacity:0.3,
+  duration:0.5,
+  ease:'power2.inOut'
+  },
+  ">-2"
+  );
+
+  tl.fromTo('.shape2',{
+  x:550,
+  y:-150,
+  },{
+  y:-185,
+  duration: 1.5,
+  repeat: -1,
+  yoyo: true,
+  ease:'sine.inOut'
+  }
+  );
+
+  tl.fromTo('.info-section h4',{
+    x: 50,
+    opacity: 0
+  },{
+    x:0,
+    opacity: 1,
+    duration: 1.3
+  },
+  "h1Show"
+  );
+
+  tl.fromTo('.call-actions',{
+    x: 50,
+    opacity: 0
+  },{
+    x:0,
+    opacity: 1,
+    duration: 1.3
+  },
+  "h1Show+=1"
+  );
+};
+
+const midScreen768 = () => {
+  let tl = gsap.timeline();
+
+  tl.fromTo('.logo',{
+    x: -200,
+    opacity: 0
+  },{
+    x:0,
+    opacity: 1,
+    delay: '.5',
+    duration: 1.3
+  });
+
+  tl.fromTo('.menu',{
+    x: 1000,
+    opacity: 0
+  },{
+    x:100,
+    opacity: 1,
+    delay: '.5',
+    duration: 1.3
+  },
+  ">-1.8"
+  );
+
+  tl.fromTo('.info-section h1',{
+    x: 0,
+    y:100,
+    opacity: 0,
+  },{
+    x:0,
+    y:0,
+    opacity: 1,
+    duration: 1.3
+  });
+  tl.addLabel("h1Show");
+
+  tl.fromTo('.girl',{
+    height: 0
+  },{
+    height:200,
+    duration: 1.4,
+    ease:'power2.inOut'
+  },
+  ">-.5"
+  );
+
+  tl.fromTo('.marketing',{
+    height: 0
+  },{
+    height:200,
+    duration: 1.4,
+    ease:'power2.inOut'
+  },
+  ">-1"
+  );
+      //Shape1
+  tl.fromTo('.shape1',{
+    x: -200,
+    opacity:0
+  },{
+    x: -100,
+    opacity: 0.3,
+    duration:1.5,
+    yoyo: true,
+    ease:'sine.inOut',
+    repeat: -1,
+  },
+  ">-1"
+  );
+
+      //Shape3
+  tl.fromTo('.shape3',{
+    opacity:0
+  },{
+    opacity:0.3,
+    duration:1,
+    ease:'power2.inOut'
+  },
+  ">-.2"
+  );
+
+  tl.fromTo('.shape3',{
+    y:400,
+    x:-200
   },{
     y:350,
     duration: 1.5,
@@ -443,7 +594,9 @@ const mobile = () => {
 if (window.screen.width <= 375){
   mobile();
 } else if(window.screen.width <= 500){
-  midScreen();
+  midScreen500();
+}else if(window.screen.width <= 768){
+  midScreen500();
 }else{
   initialPageAnimation();
 }
